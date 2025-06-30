2142221d50e029fbfdc6d58a724cfcd511eda1b6
SpuQ <sanne.santens@gmail.com>
2025-06-30 19:46:06 +0200

    Initial commit: basics for NodeRED

---
+++ nodered/settings.js
@@ -0,0 +1,74 @@
+module.exports = {
+
+    flowFile: 'flows.json',
+    flowFilePretty: true,
+
+    diagnostics: {
+        enabled: true,
+        ui: true,
+    },
+
+    runtimeState: {
+        enabled: false,
+        ui: false,
+    },
+
+    logging: {
+        console: {
+            level: "info",
+            metrics: false,
+            audit: false
+        }
+    },
+
+    functionExternalModules: true,
+    functionTimeout: 0,
+
+    exportGlobalContextKeys: false,
+
+    externalModules: {},
+
+    editorTheme: {
+        page: {
+            title: "Freya Control",
+            favicon: "/assets/favicon.ico"
+        },
+        header: {
+            title: "Freya",
+            image: "/assets/logo.png",
+            url: "/"
+        },
+        palette: {},
+        projects: {
+            enabled: false,
+            workflow: {
+                mode: "manual"
+            }
+        },
+        codeEditor: {
+            lib: "monaco",
+            options: {}
+        },
+        markdownEditor: {
+            mermaid: {
+                enabled: true
+            }
+        },
+        multiplayer: {
+            enabled: false
+        }
+    },
+
+    // Serve static files from /opt/Freya/assets
+    httpStatic: '/opt/Freya/assets',
+
+    // Network settings
+    uiPort: process.env.PORT || 1880,
+    // uiHost: "127.0.0.1", // commented to listen on all interfaces
+
+    mqttReconnectTime: 15000,
+    serialReconnectTime: 15000,
+
+    debugMaxLength: 1000,
+
+};
